2575a03af7ac43c486d9ebff753dad4eeeb2e984
SIMRAN-089 <22054089@kiit.ac.in>
2025-07-31 19:26:21 +0530

    DAY -11

--- trap_water.java
+++ trap_water.java
@@ -27,7 +27,7 @@ public static int trappedRainwater(int height[]){
      
 } 
    
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         int height [] = {4, 2, 0, 6, 3, 2, 5};
         System.out.println(trappedRainwater(height));
 
